f9a341284e32004bdbcdbdb2c49fdbd8195187c1
Matthew Harrison-Jones <matthojo@gmail.com>
2015-04-15 15:42:03 +0100

    Add in global script installs

--- scripts/global.js
+++ scripts/global.js
@@ -1,9 +1,48 @@
-var shell = require('shelljs/global'),
+var shell = require('shelljs'),
+    deasync = require('deasync'),
+    inquirer = require('inquirer'),
     chalk = require('chalk'),
 
     ok = chalk.green,
-    error = chalk.bold.red
+    info = chalk.blue,
+    error = chalk.bold.red,
+
+    config = require('../configs/global.json')
 
 exports.init = function() {
-  console.log(ok('Running Global Script'))
+  // Install packages
+  console.log(ok('Running Global Installs..'))
+  var optionalDone
+  for (var item in config.packages) {
+    var pkg = config.packages[item]
+    optionalDone = false
+
+    // Ask if package is optional
+    if (pkg.optional) {
+      inquirer.prompt([{
+        type: 'confirm'
+      , name: 'optional'
+      , message: chalk.bold.magenta('Install Optional:', chalk.reset.white(pkg.name))
+      }]
+      , function( answers ) {
+          if (answers.optional) {
+            install(pkg)
+          } else {
+            console.log(chalk.bold.blue('Skipping:', chalk.reset.white(pkg.name)))
+          }
+          optionalDone = true
+        })
+      while(!optionalDone) {
+        require('deasync').runLoopOnce()
+      }
+    } else {
+      install(pkg)
+    }
+  }
+}
+
+function install(pkg) {
+  var homeDir = (process.platform === 'win32') ? process.env.HOMEPATH : process.env.HOME;
+  console.log(info('Writing ' + pkg.name + ' to ' + homeDir + pkg.installDir + pkg.name))
+  shell.cat(__dirname + '/../configs/dotfiles/' + pkg.name).to(homeDir + pkg.installDir + pkg.name)
 }
